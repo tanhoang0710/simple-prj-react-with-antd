b1385643cd5fa848ef9c22048c77dd8faebb4c30
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-08 16:13:52 +0700

    #7 - Work on modal component

--- src/views/AppHome.js
+++ src/views/AppHome.js
@@ -2,6 +2,7 @@ import React from "react";
 import AppAbout from "../components/home/AppAbout";
 import AppFeature from "../components/home/AppFeature";
 import AppHero from "../components/home/AppHero";
+import AppWorks from "../components/home/AppWorks";
 
 export default function AppHome() {
 	return (
@@ -9,6 +10,7 @@ export default function AppHome() {
 			<AppHero />
 			<AppAbout />
 			<AppFeature />
+			<AppWorks />
 		</div>
 	);
 }
